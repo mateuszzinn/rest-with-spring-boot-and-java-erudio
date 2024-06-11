7c4b2515e89924f0c3446c4bed05a2f5e7869a13
mateuszzinn <mateuslima2002.ml@gmail.com>
2024-06-11 15:36:55 -0300

    Working with four HTTP Main Verbs

---
+++ 07_WorkingWithVerbs/src/main/java/br/com/mateus/controllers/PersonController.java
@@ -0,0 +1,44 @@
+package br.com.mateus.controllers;
+
+import br.com.mateus.exception.UnsupportedMathOperationException;
+import br.com.mateus.model.Person;
+import br.com.mateus.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id) {
+        return service.findById(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable(value = "id") String id) {
+        service.delete(id);
+    }
+}
